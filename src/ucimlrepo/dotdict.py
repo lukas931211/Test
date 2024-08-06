5d85b35eeb1052b81caf2ec762e212ebfb311828
Richard D. Paul <45046088+ripaul@users.noreply.github.com>
2024-08-06 13:59:57 +0200

    Update dotdict.py
    Added __getstate__ and __setstate__ for pickling support

--- src/ucimlrepo/dotdict.py
+++ src/ucimlrepo/dotdict.py
@@ -3,3 +3,7 @@ class dotdict(dict):
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
+
+    # requires as suggested here in order to allow for pickling the dotdict https://stackoverflow.com/a/2050357
+    def __getstate__(self): return self.__dict__
+    def __setstate__(self, d): self.__dict__.update(d)
